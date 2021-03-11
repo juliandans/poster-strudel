3bc346f2efa85ad66bbf0d1e59d0fdc496392a02
Julian Dans <gr8jld@gmail.com>
2021-03-11 15:15:41 -0800

    fixed modals

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,40 +1,55 @@
 import React from "react";
-import { Box, Badge, Image } from "@chakra-ui/react"
+import { Box, Badge, Image, useDisclosure, Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton, } from "@chakra-ui/react"
 import { ArrowUpIcon } from "@chakra-ui/icons"
 import nsfw_image_bonk from "../assets/images/nsfw_image_bonk.png"
-import PostModal from "./Components/PostModal"
 
 export default function Post(props) {
   const { NSFW } = props
-  const { onOpen } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
     return (
-      <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" onclick={onOpen}>
-      <PostModal {...props} />
-      <Image src={NSFW ? nsfw_image_bonk : props.image} />
+      <>
+      <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" onClick={onOpen}>
+        <Image src={NSFW ? nsfw_image_bonk : props.image} />
 
-      <Box p="6">
-        <Box d="flex" alignItems="baseline">
-          <Badge borderRadius="full" px="2" colorScheme="teal">
-            r/{props.subreddit}
-          </Badge>
-        </Box>
+        <Box p="6">
+          <Box d="flex" alignItems="baseline">
+            <Badge borderRadius="full" px="2" colorScheme="teal">
+              r/{props.subreddit}
+            </Badge>
+          </Box>
 
-        <Box
-          mt="1"
-          fontWeight="semibold"
-          as="h4"
-          lineHeight="tight"
-        >
-          {NSFW ? 'NSFW Post' : props.title}
-        </Box>
+          <Box
+            mt="1"
+            fontWeight="semibold"
+            as="h4"
+            lineHeight="tight"
+          >
+            {NSFW ? 'NSFW Post' : props.title}
+          </Box>
 
-        <Box d="flex" mt="2" alignItems="center">
-          <Box as="span" ml="2" color="gray.600" fontSize="sm">
-            <ArrowUpIcon/> {props.upvotes} upvotes
+          <Box d="flex" mt="2" alignItems="center">
+            <Box as="span" ml="2" color="gray.600" fontSize="sm">
+              <ArrowUpIcon/> {props.upvotes} upvotes
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </Box> 
+      </Box> 
+      <Modal isOpen={isOpen} onClose={onClose} scrollBehavior={"inside"}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{props.title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Image src={props.image} />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      </>
     )
 }
